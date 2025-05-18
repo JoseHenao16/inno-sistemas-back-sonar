717dd7b7bee164a07c1cbe107dba21fb377019e9
JuanLema14 <andres.lema1@udea.edu.co>
2025-05-18 11:42:36 -0500

    Add role-based access control to UserController methods

--- src/main/java/com/udea/fe/controller/UserController.java
+++ src/main/java/com/udea/fe/controller/UserController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,24 +15,28 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/register")
     public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
         UserDTO createdUser = userService.createUser(userDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
+   @PreAuthorize("hasRole('ADMIN') or hasRole('TEACHER')")
     @GetMapping("/all")
     public ResponseEntity<Iterable<UserDTO>> getAllUsers() {
         Iterable<UserDTO> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
+   @PreAuthorize("hasRole('ADMIN') or hasRole('TEACHER')")
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
         UserDTO user = userService.getUserByID(id);
         return ResponseEntity.ok(user);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}/edit")
     public ResponseEntity<UserDTO> updateUser(
             @PathVariable Long id,
@@ -41,7 +46,7 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}/status")
     public ResponseEntity<Void> deactivateUser(@PathVariable Long id) {
         userService.deactivateUser(id);
