bfc3fd1256ba8ce9a8ef3bf0f820ddf740c68f45
JuanLema14 <andres.lema1@udea.edu.co>
2025-06-08 22:36:44 -0500

    Implementar funciones de gestión de comentarios y envíos, incluidos DTO, servicios y controladores para gestionar envíos y asignaciones de tareas.

---
+++ src/main/java/com/udea/fe/entity/Submission.java
@@ -0,0 +1,34 @@
+package com.udea.fe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "submissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Submission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long submissionId;
+
+    private String content;
+    private String fileUrl;
+    private LocalDateTime submittedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
