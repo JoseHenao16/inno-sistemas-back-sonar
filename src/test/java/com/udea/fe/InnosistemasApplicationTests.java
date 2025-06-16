7a4cc8991e3c16d21e7968fc16d35a6b77405001
JoseHenao16 <josehenao75@gmail.com>
2025-06-16 12:26:53 -0500

    Agregar configuración del plugin maven-surefire para pruebas y ajustar propiedades de seguridad en el perfil de prueba

--- src/test/java/com/udea/fe/InnosistemasApplicationTests.java
+++ src/test/java/com/udea/fe/InnosistemasApplicationTests.java
@@ -3,13 +3,15 @@ package com.udea.fe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
 class InnosistemasApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(">>>>> Perfil de prueba activo correctamente");
 	}
 
 }
